2d68b10d72c530976de9b517ddd077960ebae587
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2021-01-01 21:14:36 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-// function hello(isReact) {
-//   if (isReact) {
-//     return <p>Hello React</p>;
-//   } else {
-//     return <p>Hello Vue.js</p>;
-//   }
-// }
+function Timer() {
+  const [count, setCount] = useState(60);
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      setCount((prevCount) => (prevCount === 0 ? (prevCount = 60) : prevCount - 1));
+    }, 1000);
+    return () => clearInterval(time);
+  }, []);
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount(60)}>ボタン</button>
+    </>
+  );
+}
 
 function App() {
-  const isReact = true;
+  const [visible, setVisible] = useState(true);
   return (
-    <div>
-      {(() => {
-        if (isReact) {
-          return <p>Hello react</p>;
-        } else {
-          return <p>Hello vue.js</p>;
-        }
-      })()}
-    </div>
+    <>
+      <button onClick={() => setVisible(!visible)}>スイッチ</button>
+      {visible ? <Timer /> : ""}
+    </>
   );
 }
 
